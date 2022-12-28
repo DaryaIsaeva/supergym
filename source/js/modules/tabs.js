935ce99200b39babe750db46e307305aaeb9f73f
Darya <dasha8562@gmail.com>
2022-12-28 12:13:34 +0300

    Правки после код-ревью

--- source/js/modules/tabs.js
+++ source/js/modules/tabs.js
@@ -2,20 +2,25 @@ const tabButtons = document.querySelectorAll('.subscriptions__buttons button');
 const tabLists = document.querySelectorAll('.subscriptions__list');
 
 const onTabButtonClick = () => {
-  tabButtons.forEach((item, index) => {
-    item.addEventListener('click', (evt) => {
-      tabButtons.forEach((btn) => {
-        btn.classList.remove('active-button');
-      });
+  if (tabButtons.length > 0) {
+    tabButtons.forEach((item, index) => {
+      item.addEventListener('click', (evt) => {
+        tabButtons.forEach((btn) => {
+          btn.classList.remove('active-button');
+        });
 
-      tabLists.forEach((list) => {
-        list.classList.remove('current-tab');
-      });
+        evt.target.classList.add('active-button');
 
-      evt.target.classList.add('active-button');
-      tabLists[index].classList.add('current-tab');
+        if (tabLists.length === tabButtons.length) {
+          tabLists.forEach((list) => {
+            list.classList.remove('current-tab');
+          });
+
+          tabLists[index].classList.add('current-tab');
+        }
+      });
     });
-  });
+  }
 };
 
 export {onTabButtonClick};
